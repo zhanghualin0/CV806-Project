428e86bf39e17b0594ecdfe5d4b457926c70f202
hualin.zhang <zhanghualin98@gmail.com>
2024-02-23 22:17:13 +0400

    CIRR image embedding save

---
+++ save_blip_embs_imgs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+# Run the Python script with the necessary arguments
+python ./tools/embs/save_blip_embs_imgs.py \
+    --image_dir ./datasets/CIRR \
+    --img_ext png \
+    --batch_size 128 \
+    --num_workers 4 \
+    --model_type large
